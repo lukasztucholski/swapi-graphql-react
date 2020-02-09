fc2f94229d4be046656b9a9011fe6495188ed537
Łukasz Tucholski <luk.tucholski@gmail.com>
2020-02-09 18:01:34 +0100

    apollo client connected with react app

--- src/index.js
+++ src/index.js
@@ -3,4 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'https://swapi.apis.guru/',
+});
+
+const AppWrapper = (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+ReactDOM.render(AppWrapper, document.getElementById('root'));
